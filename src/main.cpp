f2727ddc1d44e4e79f184d9b8c33098a997337ed
hero <oscarvallesgs@gmail.com>
2024-11-28 11:18:31 -0600

    Proyecto_hidraulica_v1

---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+//Oscar Alberto Valles Limas 20060605
+//This program works to see the frecuency between colors 
+// TCS230 or TCS3200 pins wiring to Arduino
+#define S0 18
+#define S1 19
+#define S2 21
+#define S3 22
+#define sensorOut 8 //I have to see my ESP32 to define that pin
+
+// Stores frequency read by the photodiodes
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+
+void setup() {
+  // Setting the outputs
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  // Setting the sensorOut as an input
+  pinMode(sensorOut, INPUT);
+  
+  // Setting frequency scaling to 20% we can scale it to 100% with S1 and S0 in HIGH 
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+   // Begins serial communication 
+  Serial.begin(9600);
+}
+void loop() {
+  // Setting RED (R) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Reading the output frequency
+  redFrequency = pulseIn(sensorOut, LOW);
+  
+   // Printing the RED (R) value
+  Serial.print("R = ");
+  Serial.print(redFrequency);
+  delay(100);
+  
+  // Setting GREEN (G) filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  greenFrequency = pulseIn(sensorOut, LOW);
+  
+  // Printing the GREEN (G) value  
+  Serial.print(" G = ");
+  Serial.print(greenFrequency);
+  delay(100);
+ 
+  // Setting BLUE (B) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  blueFrequency = pulseIn(sensorOut, LOW);
+  
+  // Printing the BLUE (B) value 
+  Serial.print(" B = ");
+  Serial.println(blueFrequency);
+  delay(100);
+}

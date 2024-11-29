ce08da1b4ea1aa75ba4af13b66518cc25e4848b7
hero <oscarvallesgs@gmail.com>
2024-11-29 11:34:35 -0600

    Funciones para detectar colores y calibracion

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <TCS320.h>
-
+#include <BluetoothSerial.h>
 // Definir los pines para el sensor
 #define S0 18
 #define S1 19
 #define S2 21
 #define S3 22
+#define PISTON 5
 #define SENSOR_OUT 23
 
 // Crear una instancia del sensor de color
@@ -22,15 +23,23 @@ int red, green, blue;
 // Definir los modos
 #define MODE_CALIBRATION  1
 #define MODE_NORMAL       2
+#define GREEN_DETECTION   3
 int mode = MODE_NORMAL; // Comienza en modo normal
+
+//Objeto Bluetooth
+BluetoothSerial SerialBT;
+//Declaraciones de funciones 
+void colordiferences ();
 
 void setup() {
   // Inicializar la comunicación serial
   Serial.begin(9600);
-
+  // Iniciar Bluetooth con el nombre del dispositivo
+  SerialBT.begin("ESP32_Hidraulica"); // Cambia el nombre si lo deseas
+  Serial.println("Bluetooth iniciado, listo para emparejar");
   // Inicializar el sensor
   colorSensor.begin();
-
+  pinMode(PISTON, OUTPUT);
   // Esperar una instrucción para elegir el modo
   Serial.println("Selecciona el modo:");
   Serial.println("c - Modo calibración");
@@ -49,9 +58,23 @@ void loop() {
     } else if (input == 'u') {
       mode = MODE_NORMAL;
       Serial.println("Modo normal seleccionado.");
+    } else if (input == 'g') {
+      mode = GREEN_DETECTION;
+    }
+  if (SerialBT.available() > 0){
+    String mode = SerialBT.readStringUntil('\n'); // Leer el comando hasta el salto de línea
+    // Cambiar de modo según la entrada del usuario
+    if (input == 'c') {
+      mode = MODE_CALIBRATION;
+      Serial.println("Modo calibración seleccionado.");
+    } else if (input == 'u') {
+      mode = MODE_NORMAL;
+      Serial.println("Modo normal seleccionado.");
+    } else if (input == 'g') {
+      mode = GREEN_DETECTION;
     }
   }
-
+}
   // Modo calibración
   if (mode == MODE_CALIBRATION) {
     // Leer los colores
@@ -71,13 +94,7 @@ void loop() {
   // Modo normal (uso del sensor)
   if (mode == MODE_NORMAL) {
     // Leer los colores
-    colorSensor.readAll(redColor, greenColor, blueColor);
-
-    // Remapear los valores de frecuencia a valores RGB de 0 a 255
-    redColor = map(redColor, 70, 120, 255, 0);
-    greenColor = map(greenColor, 100, 199, 255, 0);
-    blueColor = map(blueColor, 38, 84, 255, 0);
-
+    colordiferences();
     // Imprimir los valores de los colores
     Serial.print("R = ");
     Serial.print(redColor);
@@ -97,4 +114,26 @@ void loop() {
 
     delay(500);  // Espera de medio segundo para la siguiente lectura
   }
+    if(mode == GREEN_DETECTION){
+
+    colordiferences();
+
+      if(greenColor > redColor && greenColor > blueColor){
+        digitalWrite(PISTON, HIGH);
+        Serial.println(" - GREEN detected!");
+      }else{
+        digitalWrite(PISTON, LOW);
+        Serial.println(" - not GREEN detected!");
+      }
+    delay(500);
+    }
+}
+void colordiferences (){
+    colorSensor.readAll(redColor, greenColor, blueColor);
+
+    // Remapear los valores de frecuencia a valores RGB de 0 a 255
+    redColor = map(redColor, 91, 246, 221, 0);
+    greenColor = map(greenColor, 158, 194, 198, 0); //17, 84, 108, 0
+    blueColor = map(blueColor, 167, 170, 139, 0); //121, 127, 146, 0
+
 }
